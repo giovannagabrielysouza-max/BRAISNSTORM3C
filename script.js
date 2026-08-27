da1eca2c182690e8b612c02c1d36e044d99c5226
giovannagabrielysouza-max <giovanna.gabriely.souza@escola.pr.gov.br>
2026-08-27 09:57:34 -0300

    s

--- script.js
+++ script.js
@@ -43,13 +43,16 @@ const perguntas = [
     ],
   },
 ];
+
 let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta() {
 perguntaAtual = perguntas[atual];
 caixaPerguntas.textContent = perguntaAtual.enunciado;
+mostraAlternativas();
 }
+
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
